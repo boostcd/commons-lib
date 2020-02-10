8f407e8d6d22a0ad28aa8eab0e04c8e8d6d42fa1
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-02-10 14:39:00 +0000

    implementation commons lib

---
+++ src/main/java/com/estafet/openshift/boost/commons/lib/properties/PropertyUtils.java
@@ -0,0 +1,48 @@
+package com.estafet.openshift.boost.commons.lib.properties;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertyUtils {
+
+	private static PropertyUtils instance;
+	
+	private Properties prop = null;
+	
+	private PropertyUtils() {}
+
+	public String getProperty(String property) {
+		if (prop == null) {
+			prop = new Properties();
+			InputStream inputStream = null;
+			String propFileName = "integration-test.properties";
+			try {
+				inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+				if (inputStream != null) {
+					prop.load(inputStream);
+				} else {
+					throw new RuntimeException("property file '" + propFileName + "' not found in the classpath");
+				}
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			} finally {
+				try {
+					if (inputStream != null) {
+						inputStream.close();
+					}
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+		return prop.getProperty(property);
+	}
+	
+	public static PropertyUtils instance() {
+		if (instance == null) {
+			instance = new PropertyUtils();
+		}
+		return instance;
+	}
+}
