f673a0fb2a363172308a7ff7996bdc57d55e17e1
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-02-12 16:22:53 +0000

    Added API class to the commons lib

---
+++ src/main/java/com/estafet/openshift/boost/commons/lib/model/API.java
@@ -0,0 +1,24 @@
+package com.estafet.openshift.boost.commons.lib.model;
+
+import java.util.StringTokenizer;
+
+public class API {
+
+	private String version;
+
+	public API() {
+	}
+
+	public API(String version) {
+		this.version = version;
+	}
+
+	public String getVersion() {
+		StringTokenizer tokenizer = new StringTokenizer(version.replaceAll("\\-SNAPSHOT", ""), ".");
+		String p1 = tokenizer.nextToken();
+		String p2 = tokenizer.nextToken();
+		String p3 = tokenizer.nextToken();
+		return p1 + "." + p2 + "." + Integer.toString(Integer.parseInt(p3) - 1);
+	}
+
+}
