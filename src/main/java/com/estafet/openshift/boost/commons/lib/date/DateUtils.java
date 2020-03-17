b4a60fac334886e9c3850c7c3819028b2d2b2192
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-03-17 15:59:10 +0000

    Updated the date utility to validate the date and use an alternative if the supplied date is not a valid format

--- src/main/java/com/estafet/openshift/boost/commons/lib/date/DateUtils.java
+++ src/main/java/com/estafet/openshift/boost/commons/lib/date/DateUtils.java
@@ -3,9 +3,13 @@ package com.estafet.openshift.boost.commons.lib.date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Pattern;
 
 public final class DateUtils {
 
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+	private static final Pattern datePattern = Pattern.compile("\\d{4}\\-\\d{2}-\\d{2}T\\d{2}\\:\\d{2}\\:\\d{2}Z");
+	
 	public static String newDate() {
 		return DateUtils.dateFormat.format(new Date());
 	}
@@ -18,6 +22,16 @@ public final class DateUtils {
 		}
 	}
 
-	public static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+	public static Date getDate(String dateString, Date defaultDate) {
+		if (isValidDate(dateString)) {
+			return getDate(dateString);	
+		} else {
+			return defaultDate;
+		}
+	}
+	
+	public static boolean isValidDate(String dateString) {
+		return dateString != null && datePattern.matcher(dateString).matches();
+	}
 
 }
