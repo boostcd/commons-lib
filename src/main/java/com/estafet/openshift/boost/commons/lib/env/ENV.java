590692cb3a707da481d4de3102e544f83c4e7e7b
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-03-23 11:30:53 +0000

    Added environment variable defaults to commons lib

---
+++ src/main/java/com/estafet/openshift/boost/commons/lib/env/ENV.java
@@ -0,0 +1,12 @@
+package com.estafet.openshift.boost.commons.lib.env;
+
+public final class ENV {
+
+	private ENV( ) {}
+	
+	public static final String PRODUCT = System.getenv("PRODUCT");	
+	public static final String OPENSHIFT_HOST_PORT = System.getenv("OPENSHIFT_HOST_PORT");
+	public static final String OPENSHIFT_USER = System.getenv("OPENSHIFT_USER");	
+	public static final String OPENSHIFT_PASSWORD = System.getenv("OPENSHIFT_PASSWORD");
+
+}
