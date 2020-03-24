a6e0a907dcf1685fe1735af99d58cd647458717e
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-03-24 13:04:31 +0000

    added github env vars

--- src/main/java/com/estafet/openshift/boost/commons/lib/env/ENV.java
+++ src/main/java/com/estafet/openshift/boost/commons/lib/env/ENV.java
@@ -5,8 +5,13 @@ public final class ENV {
 	private ENV( ) {}
 	
 	public static final String PRODUCT = System.getenv("PRODUCT");	
+	
 	public static final String OPENSHIFT_HOST_PORT = System.getenv("OPENSHIFT_HOST_PORT");
 	public static final String OPENSHIFT_USER = System.getenv("OPENSHIFT_USER");	
 	public static final String OPENSHIFT_PASSWORD = System.getenv("OPENSHIFT_PASSWORD");
+	
+	public static final String GITHUB = System.getenv("GITHUB");
+	public static final String GITHUB_USER = System.getenv("GITHUB_USER");
+	public static final String GITHUB_PASSWORD = System.getenv("GITHUB_PASSWORD");
 
 }
